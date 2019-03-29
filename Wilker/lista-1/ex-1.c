3c09b58fda2a62361fa38e3447b79a7cbc5b938a
Wilker <Wilker.rj@gmail.com>
2019-03-29 17:28:10 -0300

    wip

---
+++ Wilker/lista-1/ex-1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprime(int *vet, int n);
+int max_seg(int *vet, int n);
+int max (int a, int b);
+
+int main(int argc, char *argv[]){
+    int n = 0;
+    do{
+        scanf("%d",&n);
+        int *numeros = (int*)malloc(sizeof(int)*n);
+        int i =0;
+        for(i = 0; i < n; i++){
+            scanf("%d",&numeros[i]); 
+        }
+        //imprime(numeros,n);
+        printf("Máximo segumentos => %d \n",max_seg(numeros,n));
+    }
+    while (n>0);
+    return 0;
+}
+
+void imprime(int *vet, int n){
+    int i = 0;
+    for(i = 0; i <n; i++){
+        printf("%d ",vet[i]);
+    }
+    printf("\n");
+}
+
+/*
+int max_seg(int *vet, int ant){
+    int ant = ant;
+    if(!vet[1])
+        return 0; 
+    if(vet[1]>vet[0]){
+        return 1+max_seg(&vet[1],ant);
+    }
+    if(vet[1]<vet[0]){
+        return 0+max+seg()
+    }
+}
+*/
+
+int max_seg(int *vet, int n){
+    int i = 0;
+    int maior = 0;
+    int atual = 0;
+    for(i = 0; i < n-1; i++){
+        if(vet[n+1] > vet[n]){
+            atual++;
+            if(maior<atual){
+                maior = atual;
+                atual = 0;
+            }
+        }else{
+            if(vet[n+1] > vet[n]){
+                atual = 0;
+            }
+        }
+    }
+}
+
+
+int max(int a, int b){
+    return b < a ? a : b;
+}
